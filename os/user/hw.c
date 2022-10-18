ae05fbcfecf5cbde8de403aa6c41a0f2599605f2
pkmLee <sc21pkm@leeds.ac.uk>
2022-10-18 13:49:19 +0100

    feat: Create hw.c, it's a user command that prints hello world in xv6 shell

---
+++ os/user/hw.c
@@ -0,0 +1,10 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+// program prints hello world to console
+int main(int argc, char **argv){
+
+	printf("Hello World\n");
+	//Terminate process
+	exit(0);
+}
