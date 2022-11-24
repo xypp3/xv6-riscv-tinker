ceffaa915e62baef3760b0e4847942bdc013b7aa
pkmLee <sc21pkm@leeds.ac.uk>
2022-11-24 16:59:47 +0000

    chkpt: malloc half done

--- os/user/mem.c
+++ os/user/mem.c
@@ -1,6 +1,6 @@
 #include "mem.h"
 
-static Header *base_head = NULL; // empty starting list of free blocks
+static Header *base_head = NULL; // empty starting list of free and used blocks
 
 Header *more_mem(int size){
     unsigned int n_pages_in_bytes = ((size/PAGE_SIZE) + 1) * PAGE_SIZE; // request at least one page as it is smallest addressable block of mem
@@ -16,94 +16,91 @@ Header *more_mem(int size){
         curr_ptr = curr_ptr->next_head;
     }
     // curr_ptr == last element in list
-    curr_ptr->next_head        = appended_block; // link to end of list
-    appended_block->next_head  = NULL; // make it new end of list
-    appended_block->size        = n_pages_in_bytes; // set size of (new block(s) - malloc_used_block_size)
-    printf("1:append_ptr: %x, sbrk: %d\n", appended_block, n_pages_in_bytes);
-    printf("2: c-%x, a-%x, as-%d, cs-%d\n", curr_ptr, appended_block, appended_block->size, size);
-    // appended_block             += appended_block->size; // move to malloc_used_block 
-    // appended_block->size        = size - sizeof(Header); // set size of  
-    for (int i = 0; i < n_pages_in_bytes; i++){
-        int add = i/2;
-        (appended_block+add)->size = 1;
-        printf("%d-> %x\n", i, appended_block+add);
+    if (size == n_pages_in_bytes){
+        curr_ptr->next_head        = appended_block; // link to end of list
+        appended_block->next_head  = NULL; // make it new end of list
+        appended_block->size       = size; // set size of (new block(s) - malloc_used_block_size)
+    }else{
+        printf("1:append_ptr: %x, sbrk: %d\n", appended_block, n_pages_in_bytes);
+        printf("2: c-%x, a-%x, as-%d, cs-%d\n", curr_ptr, appended_block, appended_block->size, size);
+        // appended_block             += appended_block->size; // move to malloc_used_block 
+        // appended_block->size        = size - sizeof(Header); // set size of  
+
+        // for (int i = 0; i < n_pages_in_bytes; i++){
+        //     int add = i/2;
+        //     (appended_block+add)->size = 1;
+        //     printf("%d-> %x\n", i, appended_block+add);
+        // }
+        
+
+        Header *used_block = (Header *) ( appended_block->size - size + ((void *)appended_block) );
+        // // printf("%x\n", appended_block->size+appended_block);
+        printf("used ptr: %x, used size: %d\n", used_block, appended_block->size-size);
+        // (appended_block+new_size)->size = size;
+
+        printf("4: c-%x, a-%x, as-%d, cs-%d\n", curr_ptr, appended_block, appended_block->size, size);
+
+        // set free
+        // used_block->is_free     = FALSE;
+        printf("pls\n");
+        appended_block->is_free = TRUE;
+        printf("here!!!!!!\n");
     }
     
-
-    Header *used_block = appended_block->size-size+appended_block;
-    // printf("%x\n", appended_block->size+appended_block);
-    printf("used ptr: %x, used size: %d\n", used_block, appended_block->size-size);
-    used_block->size = size;
-
-    printf("4: c-%x, a-%x, as-%d, cs-%d\n", curr_ptr, appended_block, appended_block->size, size);
+    
 
     return appended_block; //start of malloc_used_block
 }
 
 void *_malloc(int size){
     // sanity check
-    if(size <= 0){
+    if( !( 0 < size  && size <= ( __INT32_MAX__ - sizeof(Header) ) ) ){
         return NULL;
     }
     // start malloc from here
-    Header *prev_ptr;
-    Header *iter_ptr;
-    // long to provent overflow of MAX_INT + sizeof(Header)
+    Header * iter_ptr;
 
     unsigned int requested_size = size + sizeof(Header);  
 
-    // if free list not created, then create it
+    // if list not created, then create it
     if( base_head == NULL){
         base_head = (Header *) sbrk(sizeof(Header));
         // if more_mem errors, return NULL
         if (base_head == NULL)
             return NULL;
 
-        base_head->next_head = NULL; // base_head starts off as circular  
-        base_head->size       = sizeof(Header); 
+        base_head->next_head    = NULL; // base_head starts off as circular  
+        base_head->size         = sizeof(Header);
+        base_head->is_free      = FALSE;
     }
 
-
-    prev_ptr = base_head;
     iter_ptr = base_head->next_head; // start at base_head
-
-    while(iter_ptr != NULL){
-        // printf("s:%d, req:%d\n", iter_ptr->size, requested_size);
-        if(iter_ptr->size >= requested_size){
-            if(iter_ptr->size == requested_size){
-                // printf("here2");
-                // skip over iter_ptr in free list
-                prev_ptr->next_head = iter_ptr->next_head;
-            }else{
-                // allocate tail end to not have to move prev_ptr->next_head to a different pointer
-                printf("here3: %d, %d\n", iter_ptr->size, requested_size);
-                iter_ptr->size -= requested_size;
-                printf("here4: %d, %d\n", iter_ptr->size, requested_size);
-                // iter
-                // Header *used = iter_ptr + iter_ptr->size + 1; // move to start of allocated mem block
-                // printf("here5: %x", iter_ptr);
-                // used->size = requested_size;
-                printf("here6");
-                // todo ^^^ see if not having this causes an error when freeing most likely it will appear
-            }
-
-            return (void *)(iter_ptr + 1); // return block without head
-        }
-
-        // move through the linked list by one item
-        // printf("hi: %x\n", iter_ptr->size);
-        prev_ptr = iter_ptr;
-        iter_ptr = iter_ptr->next_head;
+    printf("%x\n", iter_ptr);
+    while (iter_ptr != NULL && iter_ptr->is_free == TRUE){
+        if(iter_ptr->size >= requested_size)
+            break; // as iter_ptr is free block of right size
+        iter_ptr = iter_ptr->next_head; // increment through list
+        printf("hi\n");
+    }
+ 
+    if(iter_ptr == NULL){
+        return (void *) (more_mem(requested_size));
+    }else{ // found a free block of big ENOUGH
+        printf("1: %d\n", iter_ptr->size);
+        iter_ptr->size     -= requested_size;
+        printf("2\n");
+        Header *used_block  = ((void *) iter_ptr) + iter_ptr->size;
+        printf("here: %x", used_block);
+        // used_block->is_free = FALSE;
+        // used_block->size    = requested_size;
+        // // unlink and link the list (iter_ptr --> used_block --> iter_ptr.next_head)
+        // used_block->next_head   = iter_ptr->next_head; // 
+        // iter_ptr->next_head     = used_block;
         
+        // return (void *) (used_block + 1);
     }
 
-    // printf("boooo\n");
-    // only called if large enough block not found
-    void *block = more_mem(requested_size);
-    if (block == NULL)
-        return NULL;
-    else 
-        return (void *)(block + 1); // return block without head
+    return NULL;
 }
 
 
